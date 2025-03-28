e744d10096592c0883b74ce9289a005bab7eb961
kareenap55 <kareenap1412@gmail.com>
2025-03-28 14:09:07 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function getFormvalue(){
 	let submit= document.getElementById("sub").addEventListener("click",()=>{
-		alert(fname.value+" " + lname.value);
+		alert(fname.value+" "+lname.value);
 	})
 }
